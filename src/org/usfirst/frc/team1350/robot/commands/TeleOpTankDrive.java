70b8e49e64166fc5901701a22a234c53c9b3bf07
Noah Lutz <noah.l98@gmail.com>
2016-01-10 10:01:34 -0500

    added Log.java and added singleton to TeleOpTankDrive and Drivetrain

--- src/org/usfirst/frc/team1350/robot/commands/TeleOpTankDrive.java
+++ src/org/usfirst/frc/team1350/robot/commands/TeleOpTankDrive.java
@@ -1,5 +1,46 @@
 package org.usfirst.frc.team1350.robot.commands;
 
-public class TeleOpTankDrive {
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TeleOpTankDrive extends Command{
+	
+	//Singleton
+	private TeleOpTankDrive instance;
+	public TeleOpTankDrive getInstance(){
+		if (instance==null){
+			instance = new TeleOpTankDrive();
+		}
+		return instance;
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/org/usfirst/frc/team1350/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1350/robot/subsystems/Drivetrain.java
@@ -1,5 +1,31 @@
 package org.usfirst.frc.team1350.robot.subsystems;
 
-public class Drivetrain {
+import org.usfirst.frc.team1350.robot.Log;
+import org.usfirst.frc.team1350.robot.commands.TeleOpTankDrive;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drivetrain extends Subsystem{
+	
+	//Singleton
+	private static Drivetrain instance;
+	public Drivetrain getInstance(){
+		if (instance==null){
+			instance = new Drivetrain();
+		}
+		return instance;
+	}
+	
+	private TeleOpTankDrive tankDrive;
+	
+	private Drivetrain(){
+		Log.info("Initialized singleton of: " + this.getClass().getCanonicalName());
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
