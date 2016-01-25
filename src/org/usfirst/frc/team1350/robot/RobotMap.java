978f4f4b2bb11c3eeb7a7f4888faa6537dc96363
Marco <Marco@10.0.10.198>
2016-01-25 15:19:34 -0500

    Swapped motorcontroller pins

--- src/org/usfirst/frc/team1350/robot/RobotMap.java
+++ src/org/usfirst/frc/team1350/robot/RobotMap.java
@@ -17,8 +17,8 @@ public class RobotMap {
     // public static int rangefinderModule = 1;
 	
 	
-	public static final int DRIVE_TRAIN_LEFT_MOTOR_CONTROLLER = 1;
-	public static final int DRIVE_TRAIN_RIGHT_MOTOR_CONTROLLER = 0;
+	public static final int DRIVE_TRAIN_LEFT_MOTOR_CONTROLLER = 0;
+	public static final int DRIVE_TRAIN_RIGHT_MOTOR_CONTROLLER = 1;
 	
 	public static final int JOYSTICK_LEFT = 0;
 	public static final int JOYSTICK_RIGHT = 1; 
