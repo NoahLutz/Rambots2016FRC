e71741b59f805134eec5f45bf21a04b876917776
noah <noah@noah-laptop.lan>
2016-01-25 13:48:17 -0800

    Changed Joystick ports

--- src/org/usfirst/frc/team1350/robot/RobotMap.java
+++ src/org/usfirst/frc/team1350/robot/RobotMap.java
@@ -20,6 +20,6 @@ public class RobotMap {
 	public static final int DRIVE_TRAIN_LEFT_MOTOR_CONTROLLER = 1;
 	public static final int DRIVE_TRAIN_RIGHT_MOTOR_CONTROLLER = 0;
 	
-	public static final int JOYSTICK_LEFT = 0;
-	public static final int JOYSTICK_RIGHT = 1; 
+	public static final int JOYSTICK_LEFT = 1;
+	public static final int JOYSTICK_RIGHT = 0; 
 }
