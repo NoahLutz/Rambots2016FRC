19b03d0e3655a70f11deeddf7c61eaa0915daeb3
Noah Lutz <noah.l98@gmail.com>
2016-02-18 19:06:34 -0800

    changed tilt from motors to linear actuator (servo)

--- src/org/usfirst/frc/team1350/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1350/robot/subsystems/Shooter.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1350.robot.subsystems;
 
 import org.usfirst.frc.team1350.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -23,16 +24,15 @@ public class Shooter extends Subsystem {
 		return instance;
 	}
 
-	public final boolean FORWARD = true;
-	public final boolean REVERSE = false;
+	public static final boolean FORWARD = true;
+	public static final boolean REVERSE = false;
 	
 	private Victor leftLauncherMotorController;
 	private Victor rightLauncherMotorController;
 	
-	private Talon leftTiltMotorController;
-	private Talon rightTiltMotorController;
+	private Servo tiltActuator;
+	private Servo ballActuator;
 	
-	//TODO: create electric solenoid thingy
 	//TODO: some sort of sensor for detecting if the ball is in the shooter
 	
 	private static double launcherTilt;
@@ -42,26 +42,23 @@ public class Shooter extends Subsystem {
 		leftLauncherMotorController = new Victor(RobotMap.SHOOTER_LEFT_MOTOR_CONTROLLER);
 		rightLauncherMotorController = new Victor(RobotMap.SHOOTER_RIGHT_MOTOR_CONTROLLER);
 		
-		leftTiltMotorController = new Talon(RobotMap.SHOOTER_TILT_LEFT_MOTOR_CONTROLLER);
-		rightTiltMotorController = new Talon(RobotMap.SHOOTER_TILT_RIGHT_MOTOR_CONTROLLER);
-		
-		//TODO: initialize solenoid thingy
+		tiltActuator = new Servo(RobotMap.SHOOTER_TILT_LEFT_ACTUATOR);
+		ballActuator = new Servo(RobotMap.SHOOTER_BALL_ACTUATOR);
 	}
 	
-	public static double getTilt(){
-		return launcherTilt;
+	public double getAngle(){
+		return tiltActuator.getAngle();
 	}
 	
-	public static void setTilt(double angle){
-		launcherTilt = angle;
+	public void setAngle(double angle){
+		tiltActuator.setAngle(angle);
 	}
 	
 	//if direction is set to false, it will run in reverse
 	public void runShooterMotor(double speed, boolean direction){
-		//adjust speed if needed
 		if(!direction){
-			//halve the speed and reverse the direction
-			speed = -(speed/2);
+			//reverse the direction
+			speed = -(speed);
 		}
 		
 		leftLauncherMotorController.set(speed);
@@ -73,26 +70,10 @@ public class Shooter extends Subsystem {
 		rightLauncherMotorController.set(0.0);
 	}
 	
-	public void runTiltMotors(double speed, boolean direction){
-		if(!direction){
-			//no need to halve the speed here
-			speed = -speed;
-		}
-		
-		leftTiltMotorController.set(speed);
-		rightTiltMotorController.set(speed);
-	}
-	
-	public void stopTiltMotors(){
-		leftTiltMotorController.set(0.0);
-		rightTiltMotorController.set(0.0);
-	}
-	
 	public boolean ballIsInShooter(){
 		//TODO: look at sensor to see if the ball is in the shooter
 		return false;
 	}
-	
 	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
